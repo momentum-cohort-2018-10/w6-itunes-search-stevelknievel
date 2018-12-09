d9b0b76882ab0baabe9693935e9166ae5249ee28
stevelknievel <marks.steven@gmail.com>
2018-12-09 14:25:21 -0500

    individual players under each track.

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 
 const searchButton = document.getElementById('search-button')
 const searchField = document.getElementById('search-field')
+const audioPlayer = $('#audio-player')
 
 
 $('#search-button').on('click', function (entity) {
     let query = $('#search-field').val()
-
+    // $('#audio-player').attr("src", event.target.src).trigger('play')
 
 $.get('https://itunes.apple.com/search?term', { term: query }, function (entity) {
     console.log(entity)
@@ -26,16 +27,11 @@ $.get('https://itunes.apple.com/search?term', { term: query }, function (entity)
 function trackHtml(track) {
     return `
         <a href="${track.trackViewUrl}">${track.trackName}</a> - Album: ${track.collectionName} 
-        <img src="${track.artworkUrl100}">
+        <img src="${track.artworkUrl100}"> 
+        <audio src="${track.previewUrl}" controls=""></audio>
     `
 }
 
 
 
-// ADDITIONAL TO ADD:
-// MUSIC PLAYER
-// TRACK NAME
-// Release year?
-// Description?
-// Album & collection information
-// Artwork
+// $('#audio-player').on('click', function ())
